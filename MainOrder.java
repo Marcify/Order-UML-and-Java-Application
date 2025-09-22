386406a7a7693fc7ede844fdc6ec70a112a526dc
Marc <entity0819@gmail.com>
2025-09-22 10:57:45 +0800

    Initial commit.

---
+++ MainOrder.java
@@ -0,0 +1,62 @@
+/*
+    Riparip, Marc Darel C.
+    Publico, Philip Leonel L.
+    BSIT WMA-3B
+    OOP
+*/
+
+import java.util.Scanner;
+
+public class MainOrder {
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+	    
+	    // INSTANTIATION OF TWO ARRAYS OF ORDER OBJECTS
+	    Order[] orders = new Order[2];
+	    
+	    // PROMPT
+	    for(int i = 0; i < orders.length; i++) {
+	        System.out.println("Customer " + (i+1) + " : ");
+	        System.out.println("Enter Customer Order Details");
+	        
+	        System.out.print("Customer No : ");
+	        int customerNumber = s.nextInt();
+	        s.nextLine();
+	        
+	        System.out.print("Customer Name : ");
+	        String customerName = s.nextLine();
+	        
+	        orders[i] = new Order(customerNumber, customerName);
+	        
+	        String[] itemDescription = new String[5];
+	        double[] itemPrice = new double[5];
+	        int[] itemQuantity = new int[5];
+	        
+	        System.out.println("Enter 5 items to purchase : ");
+	        
+	        for(int j = 1; j <= 5; j++) {
+	            System.out.print("Enter " + j + " item : ");
+	            itemDescription[j-1] = s.nextLine();
+	            
+	            System.out.print("Enter price of " + itemDescription[j-1] + " : Php ");
+	            itemPrice[j-1] = s.nextDouble();
+	            
+	            System.out.print("Enter quantity : ");
+	            itemQuantity[j-1] = s.nextInt();
+	            s.nextLine();
+	        }
+	        
+	        orders[i].setItemDescription(itemDescription);
+	        orders[i].setItemPrice(itemPrice);
+	        orders[i].setItemQuantity(itemQuantity);
+	        
+	        System.out.println();
+	    }
+	    
+	    // CALL DISPLAY() METHOD
+	    System.out.println("DISPLAYING ALL CUSTOMER ORDER DETAILS");
+	    for(Order order : orders) {
+	        order.display();
+	    }
+	}
+}
---
+++ Order.java
@@ -0,0 +1,90 @@
+/*
+    Riparip, Marc Darel C.
+    Publico, Philip Leonel L.
+    BSIT WMA-3B
+    OOP
+*/
+
+public class Order {
+    private int customerNumber;
+    private String customerName;
+    private String[] itemDescription = new String[5];
+    private double[] itemPrice = new double[5];
+    private int[] itemQuantity = new int[5];
+    
+    // CONSTRUCTOR
+    public Order(int customerNumber, String customerName) {
+        this.customerNumber = customerNumber;
+        this.customerName = customerName;
+        this.itemDescription = new String[5];
+        this.itemPrice = new double[5];
+        this.itemQuantity = new int[5];
+    }
+    
+    // SETTERS
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+    
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    
+    public void setItemDescription(String[] itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+    
+    public void setItemPrice(double[] itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+    
+    public void setItemQuantity(int[] itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+    
+    // GETTERS
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+    
+    public String getCustomerName() {
+        return customerName;
+    }
+    
+    public String[] getItemDescription() {
+        return itemDescription;
+    }
+    
+    public double[] getItemPrice() {
+        return itemPrice;
+    }
+    
+    public int[] getItemQuantity() {
+        return itemQuantity;
+    }
+    
+    // DISPLAY() METHOD
+    public void display() {
+        double total = 0;
+        double compute = 0;
+        
+        System.out.println("Displaying Customer Order Details");
+        System.out.println("Customer No : "  + customerNumber);
+        System.out.println("Customer Order of " + customerName + " : ");
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.println("    Item " + (i+1) + " : " + itemDescription[i]);
+            System.out.println("    Price : Php " + itemPrice[i]);
+            System.out.println("    Quantity : " + itemQuantity[i]);
+            System.out.println("    Sub-total price of " + itemDescription[i] + " : Php " + itemPrice[i] * itemQuantity[i]);
+            compute = itemPrice[i] * itemQuantity[i];
+            total += compute;
+        }
+        
+        System.out.println("**************************************************");
+        System.out.println("Total purchase by customer " + customerName + " : Php " + total);
+        System.out.println("**************************************************");
+        System.out.println();
+        
+    }
+}
